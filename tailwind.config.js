9d10571d9fef35e168a7aaa50456d68de5b5ea0e
mhhasanmehedi <mmmehedi53@gmail.com>
2022-10-15 22:24:56 +0600

    dark mode variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: ["./index.html"],
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {},
   },
